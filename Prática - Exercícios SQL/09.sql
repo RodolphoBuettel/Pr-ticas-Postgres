678e04c3d17625e2a51b4ff562bf17de7aba21b5
RodolphoBuettel <Rodolpho05buettel@gmail.com>
2022-12-07 21:23:53 -0300

    feat: exercicios sql

---
+++ Prática - Exercícios SQL/09.sql
@@ -0,0 +1,2 @@
+INSERT INTO "compras" ("id_cliente", "id_produto") VAL
+UES (8, 10);
---
+++ Prática - Exercícios SQL/10.sql
@@ -0,0 +1,4 @@
+INSERT INTO "compras" ("id_cliente", "id_produto") VAL
+UES (4, 8);
+INSERT INTO "compras" ("id_cliente", "id_produto") VAL
+UES (4, 8);
