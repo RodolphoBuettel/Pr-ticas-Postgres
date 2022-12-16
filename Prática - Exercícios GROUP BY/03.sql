3888ed3fe36e1e4650e68eb367fef2f35d2b3124
RodolphoBuettel <Rodolpho05buettel@gmail.com>
2022-12-16 19:01:54 -0300

    modf

--- Prática - Exercícios GROUP BY/03.sql
+++ Prática - Exercícios GROUP BY/03.sql
@@ -1,2 +1,2 @@
 SELECT users.name AS writer, COUNT(testimonials) AS "testimonialCount" FROM testimonials JOIN users
-ON users.id = 435 WHERE testimonials."writerId" = users.id GROUP BY users.name;
+ON testimonials."writerId" = users.id WHERE users.id  = 435 GROUP BY users.name;
--- Prática - Exercícios GROUP BY/04.sql
+++ Prática - Exercícios GROUP BY/04.sql
@@ -1,2 +1,2 @@
 SELECT MAX(salary) AS maximumSalary, roles.name AS "role" FROM jobs JOIN roles
-ON jobs."roleId" = roles.id GROUP BY "role" ORDER BY maximumSalary ASC;
+ON jobs."roleId" = roles.id WHERE jobs.active = true GROUP BY "role" ORDER BY maximumSalary ASC;
